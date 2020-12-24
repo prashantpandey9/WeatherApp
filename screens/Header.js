d065ec3bdd0f5783bd52070e8ee2155b9ddea40d
prashantpandey9 <prashantpandey94551@gmail.com>
2020-12-24 13:14:01 +0530

    added header

---
+++ screens/Header.js
@@ -0,0 +1,18 @@
+import * as React from 'react';
+import {Appbar, Title} from 'react-native-paper';
+import {View, Text} from 'react-native';
+const Header = () => {
+  return (
+    <Appbar.Header
+        theme={
+                colors:{
+                        primary: "#00aaff"
+                }
+        }
+    >
+      <Title>Weather App</Title>
+    </Appbar.Header>
+  );
+};
+
+export default Header;
